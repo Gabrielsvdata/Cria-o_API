4fddb92bd0d174a90005883761e7b4aa6aa830b0
Gabrielsvdata <gabrielsilvanovieira7@gmail.com>
2025-05-07 20:13:45 -0300

    alteração do dev pro prod

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv #Carregamento das variaveis de ambiente nesse arq
 load_dotenv()
 
 class Config():
-        SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_DEV') #puxa a variavel de ambiente e utiliza para a conexão
+        SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD') #puxa a variavel de ambiente e utiliza para a conexão
         SQLALCHEMY =_TACK_MODIFICATIONS=False #Otimiza as quetis no banco de dados
